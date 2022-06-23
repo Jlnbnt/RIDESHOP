a7e32da8040bc5bbdfb16114146fcc533fee61dc
jlnbnt <benat.julien@gmail.com>
2022-06-23 20:06:21 +0200

    make it better

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,8 +1,3 @@
-const allowedOrigins = [
-  "https://rideshop.netlify.app",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-  "http://localhost:3000",
-];
+const allowedOrigins = ["https://rideshop.netlify.app"];
 
 module.exports = allowedOrigins;

2f84e6275a04f8db0afde28aada4155e3440e454
jlnbnt <benat.julien@gmail.com>
2022-06-23 20:07:15 +0200

    Initial Commit

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-/* const BASE_URL = "https://rideshop.herokuapp.com"; */
-const BASE_URL = "http://localhost:3500";
+const BASE_URL = "https://rideshop.herokuapp.com";
 
 export default axios.create({
   baseURL: BASE_URL,

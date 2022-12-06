ad97e3f47ba709049a7e775c53a85aadb469ea7e
jlnbnt <benat.julien@gmail.com>
2022-12-06 20:06:28 +0100

    update axios uri

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://rideshop.herokuapp.com";
+const BASE_URL = "https://apirideshop.onrender.com";
 
 export default axios.create({
   baseURL: BASE_URL,
